62035801e7f8543b4b09f5e56d84c1f76d95d94d
Lucas Cherkewski <lucas@lucascherkewski.com>
2022-03-06 16:04:48 -0500

    Pull in saved (disk) and published (online) summaries

---
+++ update-ati-summaries.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+
+summary_col_types <- cols(
+  year = col_double(),
+  month = col_double(),
+  request_number = col_character(),
+  summary_en = col_character(),
+  summary_fr = col_character(),
+  disposition = col_character(),
+  pages = col_double(),
+  comments_en = col_character(),
+  comments_fr = col_character(),
+  umd_number = col_double(),
+  owner_org = col_character(),
+  owner_org_title = col_character()
+)
+
+saved_summaries <- read_csv(
+  "ati-summaries.csv",
+  col_types = summary_col_types
+)
+
+published_summaries <- read_csv(
+  "https://open.canada.ca/data/dataset/0797e893-751e-4695-8229-a5066e4fe43c/resource/19383ca2-b01a-487d-88f7-e1ffbc7d39c2/download/ati.csv",
+  col_types = summary_col_types
+)

71735ff3a7a3b34e21a95f9fa79872acbef9de4e
Lucas Cherkewski <lucas@lucascherkewski.com>
2022-03-13 19:47:36 -0400

    Revamp duplicate monitoring some more (#3)
    Gives us a list of all duplicates (tagged where we know the cause)
    and uncategorized ones. Counts total, categorized, uncategorized by
    field.

--- cleaning/duplicates/identify.R
+++ cleaning/duplicates/identify.R
@@ -13,11 +13,10 @@ categorized_duplicates %>%
   write_csv("cleaning/duplicates/categorized.csv")
 
 # Identify duplicates (based on `owner_org` and `request_number` combo)
-uncategorized_duplicates <- saved_summaries %>%
+duplicates <- saved_summaries %>%
   group_by(owner_org, request_number) %>%
   summarize(count = n()) %>%
-  filter(count > 1) %>%
-  anti_join(categorized_duplicates)
+  filter(count > 1)
 
 count_duplicates_by_field <- function(x, field_to_count) {
   x %>%
@@ -26,7 +25,6 @@ count_duplicates_by_field <- function(x, field_to_count) {
     group_by(owner_org, request_number) %>%
     summarize(count = n()) %>%
     filter(count > 1) %>%
-    anti_join(categorized_duplicates) %>%
     nrow()
 }
 
@@ -35,14 +33,23 @@ duplicates_by_field <- saved_summaries %>%
   .[!. %in% c("owner_org", "request_number")] %>% # remove the two columns we don't want to include when counting duplicates (because they're our unique ID columns, we hope)
   enframe(name = NULL, value = "field") %>%
   mutate(
-    count_duplicates = map_int(field, ~ saved_summaries %>% count_duplicates_by_field(.x))
+    total = map_int(field, ~ saved_summaries %>% count_duplicates_by_field(.x)),
+    categorized = map_int(field, ~ saved_summaries %>% semi_join(categorized_duplicates) %>% count_duplicates_by_field(.x)),
+    uncategorized = map_int(field, ~ saved_summaries %>% anti_join(categorized_duplicates) %>% count_duplicates_by_field(.x))
   )
 
 duplicates_by_field %>%
   write_csv("cleaning/duplicates/summary-by-field.csv")
 
-saved_summaries %>%
-  semi_join(uncategorized_duplicates) %>%
+duplicate_summaries <- saved_summaries %>%
+  semi_join(duplicates) %>%
   arrange(owner_org, request_number) %>%
-  select(owner_org, request_number, everything()) %>%
+  select(owner_org, request_number, everything())
+
+duplicate_summaries %>%
+  left_join(categorized_duplicates) %>% # add reasons for the duplicates we know of
+  write_csv("cleaning/duplicates/duplicates.csv")
+
+duplicate_summaries %>%
+  anti_join(categorized_duplicates) %>% # remove duplicates we know of
   write_csv("cleaning/duplicates/uncategorized.csv")

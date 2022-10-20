7bcf296eae74d44223744733128dcc3e235b3e8e
Lucas Cherkewski <lucas@lucascherkewski.com>
2022-10-19 21:35:31 -0400

    put logic to combine historical and current ATIs into separate script

--- load.R
+++ load.R
@@ -15,8 +15,6 @@ summary_col_types <- cols(
   owner_org_title = col_character()
 )
 
-#saved_summaries <-  read_csv("ati-full-historical-data.csv", col_types = summary_col_types)
-
 saved_summaries <- read_csv(
   "ati-summaries.csv",
   col_types = summary_col_types
